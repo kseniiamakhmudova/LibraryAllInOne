f1039d2196bf0c4779e86fb9fe5286eda39e9109
Shemsiye <xemsiye05@gmail.com>
2025-08-21 12:49:37 -0400

    Dashboard Test

---
+++ src/test/java/com/library/steps/DashboardStepDefs.java
@@ -0,0 +1,22 @@
+package com.library.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DashboardStepDefs {
+    @Given("the user logged in as {string}")
+    public void theUserLoggedInAs(String arg0) {
+        //hello
+        
+    }
+
+    @When("the user gets users, books, borrowed books numbers from UI")
+    public void theUserGetsUsersBooksBorrowedBooksNumbersFromUI() {
+
+    }
+
+    @Then("the information should be the same with the database")
+    public void theInformationShouldBeTheSameWithTheDatabase() {
+    }
+}
